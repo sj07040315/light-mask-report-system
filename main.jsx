fdb81e00f2a546ebe2eb499852bd0c2627029202
sj07040315 <114923269+sj07040315@users.noreply.github.com>
2025-05-03 16:12:12 +0800

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div className="p-4 text-xl font-bold">
+      光罩盒報工系統建置中...
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
